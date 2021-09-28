2e45720130eb16d7e971006c76c0ef55593d1872
Maximilian Goldbrunner <maximilian.goldbrunner@googlemail.com>
2021-09-28 10:48:42 +0200

    Added tasks 1 and 2, page 2

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,13 @@
 def print_variable(x):
     print(x)
+
+
+def print_calculation(x, y, sign, result):
+    print(f"Das Ergebnis von {x} {sign} {y} ist {result}!")
+
+
+def calculate_average(array_age):
+    sum = 0
+    for x in array_age:
+        sum += x
+    return sum/len(array_age)
--- test_center.py
+++ test_center.py
@@ -1,4 +1,22 @@
-from hello_world import print_variable
+from hello_world import *
 
 print_variable("Hello World")
 print_variable(123)
+
+a = 5
+b = 10
+c = 20
+d = 25
+
+addition = a+b
+subtraction = d-c
+multiplication = b*c
+division = d/a
+
+print_calculation(a, b, "+", addition)
+print_calculation(d, c, "-", subtraction)
+print_calculation(b, c, "*", multiplication)
+print_calculation(d, a, "/", division)
+
+ages = [17,18,29,10,25,30]
+print(f"Das Durchschnittsalter der {len(ages)} Personen beträgt {calculate_average(ages)} Jahre.")
