fe0105fbfbdb02afd7e1a922412b3cc6c0db721f
Maximilian Goldbrunner <maximilian.goldbrunner@googlemail.com>
2021-09-30 14:14:20 +0200

    added ue4-exercises

--- aufgaben.py
+++ aufgaben.py
@@ -5,8 +5,30 @@ def calculate_average_grade(grades):
         sum += x
     return sum/len(grades)
 
+
 def calculate_number(number):
     number *= 2
     number += 10
     number /= 2
     return number
+
+
+def get_name_from_email(email):
+    return email.split("@")[0]
+
+
+def get_name_from_provider(email):
+    return email.split("@")[1].split(".")[0]
+
+
+def join_elements_to_list(*args):
+    list = []
+    for x in args:
+        list.append(x)
+    return list
+
+
+def join_elements(args, char):
+    return char.join(args)
+
+
--- test_center.py
+++ test_center.py
@@ -1,8 +1,29 @@
 from aufgaben import *
 
+# Listen
+
 grades = [1, 2, 3, 4, 2, 1, 1]
 print(f"Der Notendurchschnitt beträgt {calculate_average_grade(grades)}.")
 
 number = 5
 print(f"[Kommazahl] Die Berechnung mit der Nummer {number} ergibt {calculate_number(number)}.")
 print(f"[Ganzzahl] Die Berechnung mit der Nummer {number} ergibt {int(calculate_number(number))}.")
+
+
+# Übungsaufgaben
+
+email = "willy.wizard@zauberschule.de"
+print(f"Der Kunde mit der Email {email} heißt {get_name_from_email(email)}.")
+
+email = "info@helena-hexe.de"
+print(f"Der Kunde mit der Email {email} heißt {get_name_from_provider(email)}.")
+
+mail1 = "zarah.zauber@zauberberg.de"
+mail2 = "info@trixie-trickser.com"
+mail3 = "uwe_unhold@dunkelnetz.de"
+
+kunden = join_elements_to_list(mail1, mail2, mail3)
+print(f"Die Kundenliste mit der Länge {len(kunden)} ist {kunden}.")
+
+zauberer = ["Buehnenzauberer", "magic.com"]
+print(f"Die Email lautet {join_elements(zauberer, '@')}.")
