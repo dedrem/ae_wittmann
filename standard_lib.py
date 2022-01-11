a5e95c799b1bda9b161cbbee70050a08708cbba1
Maximilian Goldbrunner <maximilian.goldbrunner@googlemail.com>
2022-01-11 14:33:46 +0100

    started standard lib

---
+++ standard_lib.py
@@ -0,0 +1,35 @@
+# contains all standard operations
+
+
+# gets input from user
+def user_input(prompt: str, datatype: type) -> any:
+    inp = input(prompt)
+    if datatype == str:
+        return inp
+    elif datatype == int:
+        return int(inp)
+    elif datatype == float:
+        return float(inp)
+
+
+# prints f string
+def print_f_string(string_to_print: str, *args) -> None:
+    substrings = string_to_print.split("{}")
+    output = substrings.pop(0)
+    i = 0
+    print(args[i])
+    while len(substrings) > 0:
+        output += str(args[i]) + substrings.pop(0)
+        i += 1
+    print(output)
+
+
+# sum up a list
+def sum_list(list_to_sum: list) -> any:
+    output = 0
+    for item in list_to_sum:
+        output += item
+    return output
+
+
+
--- test_center.py
+++ test_center.py
@@ -1,10 +1,12 @@
+import standard_lib
 from aufgaben import *
 
 if __name__ == "__main__":
     #get_order_price()
     #get_brutto_from_netto(20)
     #calculate_moneeeeeys()
-    print(get_total(True))
+
+
 
 
 
