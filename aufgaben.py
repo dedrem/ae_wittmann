56cd244ee2ae467561a109e74ea240ec83e67888
dedrem <73440435+dedrem@users.noreply.github.com>
2022-04-26 14:54:52 +0200

    initial task

--- aufgaben.py
+++ aufgaben.py
@@ -1,61 +1,8 @@
-from strings import *
-import standard_lib
-import math
-import locale
+import pandas 
 
-locale.setlocale(locale.LC_ALL, "de_DE")
+dataframe: pandas.DataFrame = pandas.read_csv('csv_data/flughafenkuerzel.csv', sep=';')
+print(dataframe.values.tolist())
 
-
-class ThreeDimensionalBody:
-    def surface(self) -> float:
-        pass
-
-    def volume(self) -> float:
-        pass
-
-
-class Cube(ThreeDimensionalBody):
-    def __init__(self, length_side):
-        self.length_side = length_side
-
-    def surface(self) -> float:
-        return self.length_side ** 2
-
-    def volume(self) -> float:
-        return self.length_side ** 3
-
-
-class Sphere(ThreeDimensionalBody):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def surface(self) -> float:
-        return 4 * math.pi * self.radius**2
-
-    def volume(self) -> float:
-        return (3/4) * math.pi * self.radius**3
-
-
-class Account:
-    def __init__(self, credits, pin):
-        self.credits: float = credits
-        self.pin = pin
-
-    def pay_in(self, amount) -> None:
-        self.credits += amount
-
-    def withdraw(self, amount) -> None:
-        if not self.check_pin():
-            print(error_message_pin)
-        else:
-            if amount > self.credits:
-                standard_lib.print_f_string(error_message_money, locale.currency(self.credits))
-            else:
-                self.credits -= amount
-
-    def check_pin(self) -> bool:
-        user_input = standard_lib.user_input(pin_prompt, int)
-        return True if user_input == self.pin else False
-
-
+dataframe.columns = ['city', 'postal code', 'abrv.']
+print(dataframe)
 
--- test_center.py
+++ test_center.py
@@ -1,24 +1,7 @@
-import standard_lib
 from aufgaben import *
 
 if __name__ == "__main__":
-    print("UE_7")
-
-    print("\ncube")
-    cube = Cube(5)
-    print(cube.volume())
-    print(cube.surface())
-
-    print("\nsphere")
-    sphere = Sphere(4)
-    print(sphere.volume())
-    print(sphere.surface())
-
-    print("\n Banking")
-    account = Account(1000, 1234)
-    account.pay_in(100)
-    account.withdraw(1000)
-    account.withdraw(1000)
+    p
 
 
 
